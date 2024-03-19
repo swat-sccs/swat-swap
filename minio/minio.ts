b9826da6682de4253f092d02fc62e6932f351d00
imanolympic <theronmansilla@gmail.com>
2024-03-19 16:52:57 -0400

    configure minio client

---
+++ minio/minio.ts
@@ -0,0 +1,22 @@
+import { isProduction } from "@/app/config";
+import { parsedEnv } from "@/app/env.mjs";
+import * as Minio from "minio";
+
+const minioClientSingelton = () =>
+  new Minio.Client({
+    endPoint: parsedEnv.MINIO_ENDPOINT,
+    port: parsedEnv.MINIO_PORT,
+    useSSL: parsedEnv.MINO_USE_SSL,
+    accessKey: parsedEnv.MINIO_ROOT_USER,
+    secretKey: parsedEnv.MINIO_ROOT_PASSWORD,
+  });
+
+declare global {
+  var minioClient: undefined | ReturnType<typeof minioClientSingelton>;
+}
+
+export const minioClient = globalThis.minioClient ?? minioClientSingelton();
+
+if (!isProduction) {
+  globalThis.minioClient = minioClient;
+}
