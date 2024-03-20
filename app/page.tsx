dc9b94300dfda57f43ca7c50046d43979e6cfbb0
imanolympic <theronmansilla@gmail.com>
2024-03-20 01:23:33 -0400

    switch to native div for home page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import ListingCard from "@/components/ListingCard";
 import SideBar from "@/components/SideBar";
-import { Box, Grid } from "@mui/material";
 import { getAllActiveUserListings } from "@/app/actions";
 
 export default async function Home() {
@@ -9,21 +8,14 @@ export default async function Home() {
   const listings = await getAllActiveUserListings(userId);
 
   return (
-    <Box className="flex h-[calc(100vh-68.5px)]">
-      <SideBar></SideBar>
+    <div className="flex gap-x-8">
+      <SideBar />
 
-      <Grid
-        container
-        spacing={1}
-        direction="row"
-        justifyContent="space-evenly"
-        alignItems="stretch"
-        className="flex-1 p-4 overflow-y-scroll"
-      >
+      <div className="flex flex-wrap gap-8">
         {listings.map((listing) => (
           <ListingCard key={listing.id} listing={listing} />
         ))}
-      </Grid>
-    </Box>
+      </div>
+    </div>
   );
 }
