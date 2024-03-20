90ac5a0e1ed3113b5f0701bb6f5aefa918f37be2
imanolympic <theronmansilla@gmail.com>
2024-03-20 01:24:16 -0400

    fix actions import

--- app/saved/page.tsx
+++ app/saved/page.tsx
@@ -1,14 +1,14 @@
 import ListingCard from "@/components/ListingCard";
 import SideBar from "@/components/SideBar";
 import { Box, Grid } from "@mui/material";
-import { getAllActiveListings } from "../lib/listing";
+import { getAllActiveUserListings } from "@/app/actions";
 
 export default async function Home() {
-  const listings = await getAllActiveListings();
+  const userId = 1;
+  const listings = await getAllActiveUserListings(1);
 
   return (
     <Box className="flex h-[calc(100vh-68.5px)]">
-
       <Grid
         container
         spacing={1}
