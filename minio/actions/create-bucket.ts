1ad9b8dad95cb3615fe35f29c0d2ecf751abda45
imanolympic <theronmansilla@gmail.com>
2024-03-20 01:41:33 -0400

    add read only policy to image bucket

--- minio/actions/create-bucket.ts
+++ minio/actions/create-bucket.ts
@@ -4,5 +4,24 @@ export async function createBucketIfNotExists(bucketName: string) {
   const bucketExists = await minioClient.bucketExists(bucketName);
   if (!bucketExists) {
     await minioClient.makeBucket(bucketName);
+
+    const readOnlyPolicy = {
+      Version: "2012-10-17",
+      Statement: [
+        {
+          Effect: "Allow",
+          Principal: {
+            AWS: "*",
+          },
+          Action: "s3:GetObject",
+          Resource: [`arn:aws:s3:::${bucketName}/*`],
+        },
+      ],
+    };
+
+    await minioClient.setBucketPolicy(
+      bucketName,
+      JSON.stringify(readOnlyPolicy)
+    );
   }
 }
