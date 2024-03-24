c78e073f74da0fe0c0827741cc859e05c1fd65e2
imanolympic <theronmansilla@gmail.com>
2024-03-24 18:24:01 -0400

    create toggle listing status action

---
+++ app/actions/listing/toggle-listing-status.ts
@@ -0,0 +1,16 @@
+"use server";
+import prisma from "@/prisma/prisma";
+
+export async function toggleListingActivation(
+  listingId: number,
+  active: boolean
+) {
+  await prisma.listing.update({
+    where: {
+      id: listingId,
+    },
+    data: {
+      active,
+    },
+  });
+}
