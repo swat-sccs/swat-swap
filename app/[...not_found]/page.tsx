f9ec7f1076f3487bcd0942cf811b9c51bde83870
Eli Young <eliyoung4now@gmail.com>
2024-03-29 00:29:54 -0400

    Bald:
    Bald

--- app/[...not_found]/page.tsx
+++ app/[...not_found]/page.tsx
@@ -8,7 +8,7 @@ export default function NotFound() {
     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '50px' }}>
       <Typography variant='h2'>Oopsies! Page not found!</Typography>
       <div style={{ display: 'flex', justifyContent: 'center', marginTop: '20px' }}>
-        <Image src="/static/images/cards/uni.jpg" alt="Page not found" width={400} height={300} />
+        <Image src="/images/cards/BaldConfused.jpg" alt="Page not found" width={300} height={168} />
       </div>
       <div style={{ marginTop: '20px' }}>
         <Link href="/"><Typography variant='h2'>Go back to Home</Typography></Link>
