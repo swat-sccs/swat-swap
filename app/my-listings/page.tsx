0e7d61ed1f17592b0a8d93129404a9beb7a807f0
imanolympic <theronmansilla@gmail.com>
2024-06-22 15:33:18 -0700

    update my listings page

--- app/my-listings/page.tsx
+++ app/my-listings/page.tsx
@@ -12,17 +12,34 @@ export default async function Home() {
 
   const userListings = await getUserCreatedListings(userId);
 
+  const activeListings = userListings.filter((listing) => listing.active);
+  const inactiveListings = userListings.filter((listing) => !listing.active);
+
   return (
-    <div className="flex gap-x-8">
-      <div className="flex flex-wrap gap-8">
-        {userListings.length === 0 ? (
-          <ZeroListingsNotice />
-        ) : (
-          userListings.map((listing) => (
-            <UserListingCard key={listing.id} listing={listing} />
-          ))
-        )}
-      </div>
+    <div className="flex flex-col gap-y-16 p-8">
+      {userListings.length === 0 ? (
+        <ZeroListingsNotice />
+      ) : (
+        <>
+          <div className="flex flex-col gap-y-4">
+            <p className="font-semibold text-2xl">Currently Listed</p>
+            <div className="flex flex-wrap gap-8">
+              {activeListings.map((listing) => (
+                <UserListingCard key={listing.id} listing={listing} />
+              ))}
+            </div>
+          </div>
+
+          <div className="flex flex-col gap-y-4">
+            <p className="font-semibold text-2xl">Ready to be Listed</p>
+            <div className="flex flex-wrap gap-8">
+              {inactiveListings.map((listing) => (
+                <UserListingCard key={listing.id} listing={listing} />
+              ))}
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 }
