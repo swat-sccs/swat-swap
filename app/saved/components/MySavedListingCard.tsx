257c1558d2d7278e5c4051045eacc7545487d23a
imanolympic <theronmansilla@gmail.com>
2024-06-30 16:00:14 -0400

    wrap card with link

--- app/saved/components/MySavedListingCard.tsx
+++ app/saved/components/MySavedListingCard.tsx
@@ -13,33 +13,33 @@ export default function MySavedListingCard({
   listing,
 }: MySavedListingCardProps) {
   return (
-    <div className="flex flex-col p-4 shadow-md rounded-lg h-fit gap-y-2">
-      <div className="flex justify-between">
-        <Link href={`listing/${listing.id}`}>
+    <Link href={`listing/${listing.id}`}>
+      <div className="flex flex-col p-4 shadow-md rounded-lg h-fit gap-y-2">
+        <div className="flex justify-between">
           <p className="text-xl font-semibold">{listing.title}</p>
-        </Link>
-        {listing.price !== null && (
-          <p className="font-semibold">${listing.price}</p>
-        )}
-      </div>
-
-      {listing.images.map((image) => (
-        <div
-          key={image.id}
-          className="flex flex-col relative min-w-64 min-h-64 p-8 overflow-hidden"
-        >
-          <Image
-            fill={true}
-            className="object-cover"
-            src={`http://${minioEndpoint}:${minioPort}/${listingImagesBucketName}/${image.fileName}`}
-            alt="listing image featuring a product/service"
-          />
+          {listing.price !== null && (
+            <p className="font-semibold">${listing.price}</p>
+          )}
         </div>
-      ))}
-      <div className="flex justify-end">
-        <ListingTypePill type={listing.type} />
-        <FavoriteListingIcon listing={listing} />
+
+        {listing.images.map((image) => (
+          <div
+            key={image.id}
+            className="flex flex-col relative min-w-64 min-h-64 p-8 overflow-hidden"
+          >
+            <Image
+              fill={true}
+              className="object-cover"
+              src={`http://${minioEndpoint}:${minioPort}/${listingImagesBucketName}/${image.fileName}`}
+              alt="listing image featuring a product/service"
+            />
+          </div>
+        ))}
+        <div className="flex justify-end">
+          <ListingTypePill type={listing.type} />
+          <FavoriteListingIcon listing={listing} />
+        </div>
       </div>
-    </div>
+    </Link>
   );
 }
