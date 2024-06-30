05a51e5fc2991a400bd1d90aecf6761e5d9ad336
imanolympic <theronmansilla@gmail.com>
2024-06-30 15:53:07 -0400

    rename refactor

--- utils/hooks/user/get-user.ts
+++ utils/hooks/user/get-user.ts
@@ -1,10 +1,10 @@
-import { getUserById } from "@/app/actions";
+import { getUserDataById } from "@/app/actions";
 import { useQuery } from "@tanstack/react-query";
 import { useCallback } from "react";
 
 export function useGetBareUserById(userId: number) {
   async function fetchBareUserByIdFn(userId: number) {
-    return getUserById(userId);
+    return getUserDataById(userId);
   }
   return useQuery({
     queryKey: ["user", userId],
