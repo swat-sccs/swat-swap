f647ce4b237bc75ee9737fd458aec65c1a136c29
imanolympic <theronmansilla@gmail.com>
2024-07-14 03:19:20 -0400

    fix edit profile route

--- constants/routes.ts
+++ constants/routes.ts
@@ -4,6 +4,6 @@ export const MY_LISTINGS = "/my-listings";
 export const SAVED_LISTINGS = "/saved";
 export const CREATE_LISTING = "/create";
 
-export const PROFILE = "/profile";
+export const PROFILE = "/profile/edit";
 
 export const SIGN_OUT = "/api/auth/signout";
