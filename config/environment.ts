ef78fbbf60d03528a546f0b6e9201d4486bd6e5e
imanolympic <theronmansilla@gmail.com>
2024-07-14 02:06:05 -0400

    refactor environment config

--- config/environment.ts
+++ config/environment.ts
@@ -1,13 +1,10 @@
-import { parsedEnv } from "@/app/env.mjs";
-
-export const nodeEnvironment = parsedEnv.NODE_ENV;
+export const nodeEnvironment = process.env.NODE_ENV;
 export const isProduction = nodeEnvironment === "production";
 export const isDevelopment = nodeEnvironment === "development";
 
-export const minioEndpoint = parsedEnv.MINIO_ENDPOINT;
-export const minioPort = parsedEnv.MINIO_PORT;
-export const minioUseSSL = parsedEnv.MINO_USE_SSL;
-export const minioRootUser = parsedEnv.MINIO_ROOT_USER;
-export const minioRootPassword = parsedEnv.MINIO_ROOT_PASSWORD;
-
-export const listingImagesBucketName = parsedEnv.LISTING_IMAGES_BUCKET_NAME;
+export const minioEndpoint = process.env.MINIO_ENDPOINT;
+export const minioPort = process.env.MINIO_PORT;
+export const minioUseSSL = process.env.MINO_USE_SSL;
+export const minioRootUser = process.env.MINIO_ROOT_USER;
+export const minioRootPassword = process.env.MINIO_ROOT_PASSWORD;
+export const listingImagesBucketName = process.env.LISTING_IMAGES_BUCKET_NAME;
