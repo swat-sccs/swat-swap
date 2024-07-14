6fb816ef2b5c1c530b7e4ac7b931539ef7e33bee
imanolympic <theronmansilla@gmail.com>
2024-07-14 04:03:22 -0400

    update deps and refactor providers

---
+++ app/components/PublicListingCard.tsx
@@ -0,0 +1,45 @@
+import FavoriteListingIcon from "@/components/FavoriteListingIcon";
+import ListingTypePill from "@/components/ListingTypePill";
+import { listingImagesBucketName, minioHostname, minioPort } from "@/config";
+import { SavedListing } from "@/dtos";
+import Image from "next/image";
+import Link from "next/link";
+
+export interface ListingCardProps {
+  listing: SavedListing;
+}
+
+const PublicListingCard = ({ listing }: ListingCardProps) => {
+  return (
+    <Link href={`listing/${listing.id}`}>
+      <div className="flex flex-col p-4 shadow-md rounded-lg h-fit gap-y-2 hover:cursor-pointer">
+        <div className="flex justify-between">
+          <p className="text-xl font-semibold">{listing.title}</p>
+          {listing.price !== null && (
+            <p className="font-semibold">${listing.price}</p>
+          )}
+        </div>
+
+        {listing.images.map((image) => (
+          <div
+            key={image.id}
+            className="flex flex-col relative min-w-64 min-h-64 p-8 overflow-hidden"
+          >
+            <Image
+              fill={true}
+              className="object-cover"
+              src={`http://${minioHostname}:${minioPort}/${listingImagesBucketName}/${image.fileName}`}
+              alt="listing image featuring a product/service"
+            />
+          </div>
+        ))}
+        <div className="flex justify-end">
+          <ListingTypePill type={listing.type} />
+          <FavoriteListingIcon listing={listing} />
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+export default PublicListingCard;
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,8 @@
-import SideBar from "@/components/SideBar";
 import { getAvailableListings } from "@/app/actions";
 import { getSessionUserId } from "@/utils/hooks";
-import { type SavedListing, savedListingsSchema } from "@/dtos";
-import Link from "next/link";
-import Image from "next/image";
-import { listingImagesBucketName, minioEndpoint, minioPort } from "@/config";
-import ListingTypePill from "@/components/ListingTypePill";
-import FavoriteListingIcon from "@/components/FavoriteListingIcon";
-import prisma from "@/prisma/prisma";
+import prisma from "@/prisma/db";
+import { SavedListing, savedListingsSchema } from "@/dtos";
+import PublicListingCard from "./components/PublicListingCard";
 
 const getHomeListings = async (userId: number): Promise<SavedListing[]> => {
   const availableListings = await getAvailableListings(userId);
@@ -31,57 +26,19 @@ const getHomeListings = async (userId: number): Promise<SavedListing[]> => {
   return validatedListings;
 };
 
-export interface ListingCardProps {
-  listing: SavedListing;
-}
-
-const ListingCard = ({ listing }: ListingCardProps) => {
-  return (
-    <Link href={`listing/${listing.id}`}>
-      <div className="flex flex-col p-4 shadow-md rounded-lg h-fit gap-y-2 hover:cursor-pointer">
-        <div className="flex justify-between">
-          <p className="text-xl font-semibold">{listing.title}</p>
-          {listing.price !== null && (
-            <p className="font-semibold">${listing.price}</p>
-          )}
-        </div>
-
-        {listing.images.map((image) => (
-          <div
-            key={image.id}
-            className="flex flex-col relative min-w-64 min-h-64 p-8 overflow-hidden"
-          >
-            <Image
-              fill={true}
-              className="object-cover"
-              src={`http://${minioEndpoint}:${minioPort}/${listingImagesBucketName}/${image.fileName}`}
-              alt="listing image featuring a product/service"
-            />
-          </div>
-        ))}
-        <div className="flex justify-end">
-          <ListingTypePill type={listing.type} />
-          <FavoriteListingIcon listing={listing} />
-        </div>
-      </div>
-    </Link>
-  );
-};
-
 export default async function Home() {
   const userId = await getSessionUserId();
   if (!userId) {
     return <div>Not logged in</div>;
   }
 
-  const listings: SavedListing[] = await getHomeListings(userId);
+  const listings = await getHomeListings(userId);
 
   return (
-    <div className="flex gap-x-8">
-      <SideBar />
+    <div className="flex gap-x-8 h-full">
       <div className="flex flex-wrap gap-8">
         {listings.map((listing) => (
-          <ListingCard key={listing.id} listing={listing} />
+          <PublicListingCard key={listing.id} listing={listing} />
         ))}
       </div>
     </div>
