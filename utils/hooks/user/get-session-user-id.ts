ac9d55355b7d4fc4ff6661a39f4d945471a1fb74
imanolympic <theronmansilla@gmail.com>
2024-07-14 03:17:16 -0400

    remove console log

--- utils/hooks/user/get-session-user-id.ts
+++ utils/hooks/user/get-session-user-id.ts
@@ -2,6 +2,5 @@ import { getSessionUser } from "./get-session-user";
 
 export async function getSessionUserId() {
   const user = await getSessionUser();
-  console.log(user);
   return user ? Number(user.id) : undefined;
 }
