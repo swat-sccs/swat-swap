e460034b6594e2ef9f79eca3999be508279355ff
imanolympic <theronmansilla@gmail.com>
2024-07-20 23:52:26 -0400

    [refactor]: use native p tag

--- app/[...not_found]/page.tsx
+++ app/[...not_found]/page.tsx
@@ -1,22 +1,36 @@
-import Link from 'next/link';
-import Image from 'next/image';
-import AsciiMeme from '@/components/AsciiMeme';
-import { Typography } from '@mui/material';
+import Link from "next/link";
+import Image from "next/image";
+import AsciiMeme from "@/components/AsciiMeme";
 
 export default function NotFound() {
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '50px' }}>
-      <Typography variant='h2'>Oopsies! Page not found!</Typography>
-      <div style={{ display: 'flex', justifyContent: 'center', marginTop: '20px' }}>
-        <Image src="/images/cards/BaldConfused.jpg" alt="Page not found" width={300} height={168} />
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        marginTop: "50px",
+      }}
+    >
+      <p>Oopsies! Page not found!</p>
+      <div
+        style={{ display: "flex", justifyContent: "center", marginTop: "20px" }}
+      >
+        <Image
+          src="/images/cards/BaldConfused.jpg"
+          alt="Page not found"
+          width={300}
+          height={168}
+        />
       </div>
-      <div style={{ marginTop: '20px' }}>
-        <Link href="/"><Typography variant='h2'>Go back to Home</Typography></Link>
+      <div style={{ marginTop: "20px" }}>
+        <Link href="/">
+          <p>Go back to Home</p>
+        </Link>
       </div>
-      <div style={{ marginTop: '20px' }}>
-        <AsciiMeme/>
+      <div style={{ marginTop: "20px" }}>
+        <AsciiMeme />
       </div>
-
     </div>
   );
 }
