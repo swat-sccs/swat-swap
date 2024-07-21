71fc25de2ffbf648b1558b8165396879d29083a8
imanolympic <theronmansilla@gmail.com>
2024-07-21 15:10:18 -0400

    setup mantine properly in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <ColorSchemeScript />
+      </head>
       <body>
         <NextAuthProvider>
           <TanstackQueryClientProvider>
